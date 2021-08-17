64efea33534f70e23331be0258d057a48d591888
shariarsibbir <shariarsibbirr@gmail.com>
2021-08-17 12:58:05 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.getElementById('btn-change').addEventListener('click',function(){
 document.getElementById('about-developer').addEventListener('click',function(){
     // console.log("Hey What's up guys. I am Sibbir");
     const newParagraph = document.createElement('p');
-    newParagraph.classList.add('text-center', 'bg-indigo-400', 'text-lg', 'text-white', 'mx-56', 'my-2');
+    newParagraph.classList.add('text-center', 'bg-indigo-400', 'text-lg', 'text-white', 'mx-12',);
     newParagraph.innerText = "তুই কি আমার কথা জিজ্ঞাইতাছোস?🐸🥴";
     const developerPlace = document.getElementById('developer-place');
     developerPlace.appendChild(newParagraph);
