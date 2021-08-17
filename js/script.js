d37ffaf8a9edfeb01207cadeb46e32a737591e79
shariarsibbir <shariarsibbirr@gmail.com>
2021-08-17 12:47:59 +0600

    javascript trailer added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Add event handler in button to change an text
+document.getElementById('btn-change').addEventListener('click',function(){
+    const headingText = document.getElementById('heading-text');
+    headingText.innerText = 'I am Penguin';
+});
+
+//Extra :
+document.getElementById('about-developer').addEventListener('click',function(){
+    // console.log("Hey What's up guys. I am Sibbir");
+    const newParagraph = document.createElement('p');
+    newParagraph.classList.add('text-center', 'bg-indigo-400', 'text-lg', 'text-white', 'mx-56', 'my-2');
+    newParagraph.innerText = "তুই কি আমার কথা জিজ্ঞাইতাছোস?🐸🥴";
+    const developerPlace = document.getElementById('developer-place');
+    developerPlace.appendChild(newParagraph);
+})
